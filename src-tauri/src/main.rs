7e1d27e53d5a09b456508af3938408a75d68ae3a
Rhishavhere <rhishh.me@gmail.com>
2025-08-20 13:37:22 +0530

    update main changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,31 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Declare the modules that make up the application logic.
-mod commands;
 mod platform;
-mod utils;
+mod commands;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        // Register all the invokable commands from the `commands` module.
         .invoke_handler(tauri::generate_handler![
-            commands::follow_magic_dot,
-            commands::pin_magic_dot,
-            commands::start_window_watch,
-            commands::close_magic_dot,
-            commands::close_magic_chat,
-            commands::stick_chat_to_dot,
-            commands::animate_chat_expand,
-            commands::center_magic_dot,
-            commands::toggle_magic_dot,
-            commands::set_magic_dot_creation_enabled,
-            commands::show_magic_dot,
-            commands::set_auto_show_on_copy_enabled,
-            commands::get_auto_show_on_copy_enabled
-            ,commands::set_auto_show_on_selection_enabled
-            ,commands::get_auto_show_on_selection_enabled
+            commands::get_platform,
+            commands::open_path_or_url,
+            commands::spawn,
+            commands::run,
+            commands::get_paths,
+            commands::window_set_always_on_top,
+            commands::window_set_decorations,
+            commands::window_set_shadow,
+            commands::window_resize,
+            commands::window_move,
+            commands::quit_app,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
