b8a358552e7e8fc36f80097dfcd3d18cc056a8e4
Rhishavhere <rhishh.me@gmail.com>
2025-08-20 13:37:33 +0530

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 5174,
+    port: 5175,
     strictPort: true,
     host: host || false,
     hmr: host
